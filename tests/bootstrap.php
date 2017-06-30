322215fba1f11940127e4281ee3999a38da6f280
Dorian Savina <d.savina@thecodingmachine.com>
2017-06-30 19:27:02 +0200

    [tests] test DB parsers
    Signed-off-by: Dorian Savina <d.savina@thecodingmachine.com>

---
+++ tests/bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+
+require_once __DIR__.'../../vendor/thecodingmachine/metahydrator/tests/FooBar.php';
+require_once __DIR__.'../../vendor/thecodingmachine/metahydrator/tests/Database/StubDBProvider.php';
